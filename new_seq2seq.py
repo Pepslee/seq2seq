438619ae4566445fe35346ef434d619d1cbd9fcf
Sergey Panchenko <sergey.panchenko@eos.com>
2018-10-04 16:17:43 +0300

    fix bug in decoder input

--- new_seq2seq.py
+++ new_seq2seq.py
@@ -66,23 +66,15 @@ class Data(object):
         ret_x = np.array(batch_x)
         ret_y = np.array(batch_y)
         ret_x, ret_y = normalize(ret_x, ret_y)
-
-        # if win_y == 1:
-        # ret_y = np.expand_dims(ret_y, -1)
-        # ret_y = ret_y > 0
-        # ret_x = ret_x > 0
         return ret_x, ret_y
 
 
 def normalize(X, Y=None):
     mean = np.expand_dims(np.average(X, axis=1) + 0.00001, axis=1)
     stddev = np.expand_dims(np.std(X, axis=1) + 0.00001, axis=1)
-    # print (mean.shape, stddev.shape)
-    # print (X.shape, Y.shape)
     X = X - mean
     X = X / (2.5 * stddev)
     if Y is not None:
-        # assert Y.shape == X.shape, (Y.shape, X.shape)
         Y = Y - mean
         Y = Y / (2.5 * stddev)
         return X, Y
@@ -90,7 +82,7 @@ def normalize(X, Y=None):
 
 
 def generate_x_y_data(isTrain, batch_size, l_x, l_y):
-    path = 'hullma_1h_90d.csv'
+    path = '/home/panchenko/PycharmProjects/seq2seq/hullma_3m_180d.csv'
     data = Data(path, l_x, l_y)
     if isTrain:
         batch_xs, batch_ys = data.get_batch(data.train, data.ind_train, data.train_batch_position, batch_size, data.win_x,
@@ -103,222 +95,149 @@ def generate_x_y_data(isTrain, batch_size, l_x, l_y):
     return batch_xs, batch_ys
 
 
-def train_batch(batch_size):
-    X, Y = generate_x_y_data(isTrain=True, batch_size=batch_size, l_x=encoder_seq_length, l_y=decoder_seq_length )
-    feed_dict = {enc_inp[t]: X[t] for t in range(len(enc_inp))}
-    feed_dict.update({expected_sparse_output[t]: Y[t] for t in range(len(expected_sparse_output))})
-    _, loss_t = sess.run([train_op, loss], feed_dict)
-    return loss_t
+def main():
+    # input dimensions
+    batch_size = 1
+    output_dim = 1   # Output dimension (e.g.: multiple signals at once, tied in time)
+    hidden_dim = 20  # Count of hidden neurons in the recurrent units.
+
+    # Optmizer:
+    learning_rate = 0.005  # Small lr helps not to diverge during training.
+    nb_iters = 2000  # How many times we perform a training step (therefore how many times we show a batch).
+    lambda_l2_reg = 0.003  # L2 regularization of weights - avoids overfitting
+
+    # NN size
+    encoder_seq_length = 50
+    decoder_seq_length = 5
+
+    tf.reset_default_graph()
+    sess = tf.InteractiveSession()
+
+    with tf.variable_scope('Seq2seq'):
+
+        cells = list()
+        cells.append(tf.contrib.rnn.GRUCell(hidden_dim))
+        cells.append(tf.contrib.rnn.GRUCell(output_dim))
+        cell = tf.contrib.rnn.MultiRNNCell(cells)
+
+        ### encoder
+        encoder_input = tf.placeholder(shape=(None, None, 1), dtype=tf.float32, name="enc_inp")
+        encoder_output, encoder_state = tf.nn.dynamic_rnn(cell=cell, inputs=encoder_input, dtype=tf.float32)
+        expected_sparse_output = tf.placeholder(shape=(None, None, 1), dtype=tf.float32, name="expected_sparse_output")
+
+        ### training decoder
+        decoder_input = tf.placeholder(shape=(None, None, 1), dtype=tf.float32, name="dec_inp")
+        decoder_lengths = tf.placeholder(shape=(None,), dtype=tf.int32, name='decoder_lengths')
+        helper = tf.contrib.seq2seq.ScheduledOutputTrainingHelper(decoder_input, decoder_lengths, sampling_probability=0.0)
+        decoder = tf.contrib.seq2seq.BasicDecoder(cell, helper, encoder_state)
+        final_outputs, _final_state, _final_sequence_lengths = tf.contrib.seq2seq.dynamic_decode(decoder)
+        output_scale_factor = tf.Variable(1.0, name="Output_ScaleFactor")
+        # output_scale_factor = 1.0
+        output = output_scale_factor*final_outputs.rnn_output
+        ### Loss
+        output_loss = tf.reduce_mean(tf.nn.l2_loss(output - expected_sparse_output))
+
+        # Inference Decoder
+        inference_helper = tf.contrib.seq2seq.ScheduledOutputTrainingHelper(decoder_input, decoder_lengths, sampling_probability=1.0)
+        inference_decoder = tf.contrib.seq2seq.BasicDecoder(cell, inference_helper, encoder_state)
+        inference_final_outputs, inference_final_state, inference_final_sequence_lengths = tf.contrib.seq2seq.dynamic_decode(inference_decoder)
+        inference_logits = inference_final_outputs.rnn_output*output_scale_factor
+
+        reg_loss = 0
+        for tf_var in tf.trainable_variables():
+            if not ("Bias" in tf_var.name or "Output_" in tf_var.name):
+                reg_loss += tf.reduce_mean(tf.nn.l2_loss(tf_var))
+
+        loss = output_loss + lambda_l2_reg * reg_loss
+
+        with tf.variable_scope('Optimizer'):
+            optimizer = tf.train.AdamOptimizer(learning_rate)
+            train_op = optimizer.minimize(loss)
+
+        sess.run(tf.global_variables_initializer())
+
+        # Training
+        train_losses = []
+        test_losses = []
+        for t in range(nb_iters + 1):
+
+            X, Y = generate_x_y_data(isTrain=True, batch_size=batch_size, l_x=encoder_seq_length, l_y=decoder_seq_length)
+
+            decoder_input_np = np.concatenate((X[:, -1:, :], Y[:, :-1, :]), axis=1)
+            feed_dict = {encoder_input: X, expected_sparse_output: Y, decoder_input: decoder_input_np, decoder_lengths: [decoder_seq_length]}
+            _, train_loss, logits_np = sess.run([train_op, loss, encoder_output], feed_dict)
+            train_losses.append(train_loss)
+
+            if t % 10 == 0:
+                # Tester
+                X, Y = generate_x_y_data(isTrain=False, batch_size=batch_size, l_x=encoder_seq_length,
+                                         l_y=decoder_seq_length)
+                feed_dict = {encoder_input: X, expected_sparse_output: Y, decoder_input: decoder_input_np,
+                             decoder_lengths: [decoder_seq_length]}
+                _, test_loss, logits_np_test = sess.run([train_op, loss, encoder_output], feed_dict)
+
+                test_losses.append(test_loss)
+                print("Step {}/{}, train loss: {}, \tTEST loss: {}".format(t, nb_iters, train_loss, test_loss))
+
+        print 'Done'
+
+
+    ### Build some diagrams
+
+    plt.figure(figsize=(12, 6))
+    plt.plot(
+        np.array(range(0, len(test_losses))) / float(len(test_losses) - 1) * (len(train_losses) - 1),
+        np.log(test_losses),
+        label="Test loss"
+    )
+    plt.plot(
+        np.log(train_losses),
+        label="Train loss"
+    )
+    plt.title("Training errors over time (on a logarithmic scale)")
+    plt.xlabel('Iteration')
+    plt.ylabel('log(Loss)')
+    plt.legend(loc='best')
+    plt.show()
 
 
+    # Test
+    nb_predictions = 50
+    print("Let's visualize {} predictions with our signals:".format(nb_predictions))
 
-def test_batch(batch_size):
-    X, Y = generate_x_y_data(isTrain=False, batch_size=batch_size, l_x=encoder_seq_length, l_y=decoder_seq_length)
-    feed_dict = {enc_inp[t]: X[t] for t in range(len(enc_inp))}
-    feed_dict.update({expected_sparse_output[t]: Y[t] for t in range(len(expected_sparse_output))})
-    loss_t = sess.run([loss], feed_dict)
-    return loss_t[0]
-
-
-
-def train_batch_new(batch_size):
-    X, Y = generate_x_y_data(isTrain=True, batch_size=batch_size, l_x=encoder_seq_length, l_y=decoder_seq_length )
-    feed_dict = {enc_inp[t]: X[t] for t in range(len(enc_inp))}
-    feed_dict.update({expected_sparse_output[t]: Y[t] for t in range(len(expected_sparse_output))})
-    _, loss_t = sess.run([train_op, loss], feed_dict)
-    return loss_t
-
-encoder_seq_length = 40
-decoder_seq_length = 5
-
-
-batch_size = 1
-
-sample_x, sample_y = generate_x_y_data(isTrain=True, batch_size=batch_size, l_x=encoder_seq_length, l_y=decoder_seq_length)
-print("Dimensions of the dataset for 3 X and 3 Y training examples : ")
-print(encoder_seq_length)
-print(decoder_seq_length)
-print("(seq_length, batch_size, output_dim)")
-
-# Internal neural network parameters
-
-
-output_dim = sample_y.shape[-1]
-input_dim = sample_x.shape[-1]  # Output dimension (e.g.: multiple signals at once, tied in time)
-hidden_dim = 20  # Count of hidden neurons in the recurrent units.
-layers_stacked_count = 2  # Number of stacked recurrent cells, on the neural depth axis.
-
-# Optmizer:
-learning_rate = 0.007  # Small lr helps not to diverge during training.
-nb_iters = 2000  # How many times we perform a training step (therefore how many times we show a batch).
-lr_decay = 0.92  # default: 0.9 . Simulated annealing.
-momentum = 0.5  # default: 0.0 . Momentum technique in weights update
-lambda_l2_reg = 0.003  # L2 regularization of weights - avoids overfitting
-
-
-try:
-    tf.nn.seq2seq = tf.contrib.legacy_seq2seq
-    tf.nn.rnn_cell = tf.contrib.rnn
-    tf.nn.rnn_cell.GRUCell = tf.contrib.rnn.GRUCell
-    print("TensorFlow's version : 1.0 (or more)")
-except:
-    print("TensorFlow's version : 0.12")
-
-tf.reset_default_graph()
-sess = tf.InteractiveSession()
-
-with tf.variable_scope('Seq2seq'):
-
-
-    cells = []
-    cells.append(tf.nn.rnn_cell.GRUCell(hidden_dim))
-    cells.append(tf.nn.rnn_cell.GRUCell(output_dim))
-    cell = tf.nn.rnn_cell.MultiRNNCell(cells)
-
-    encoder_input = tf.placeholder(shape=(None, None, 1), dtype=tf.float32, name="enc_inp")
-
-    encoder_output, encoder_state = tf.nn.dynamic_rnn(cell=cell, inputs=encoder_input, dtype=tf.float32)
-
-    expected_sparse_output = tf.placeholder(shape=(None, None, 1), dtype=tf.float32, name="expected_sparse_output")
-
-    decoder_input = tf.placeholder(shape=(None, None, 1), dtype=tf.float32, name="dec_inp")
-    decoder_lengths = tf.placeholder(shape=(None,), dtype=tf.int32, name='decoder_lengths')
-
-    helper = tf.contrib.seq2seq.TrainingHelper(decoder_input, decoder_lengths)
-
-    decoder = tf.contrib.seq2seq.BasicDecoder(cell, helper, encoder_state)
-
-    final_outputs, _final_state, _final_sequence_lengths = tf.contrib.seq2seq.dynamic_decode(decoder)
-
-    logits = final_outputs.rnn_output
-
-    output_scale_factor = tf.Variable(1.0, name="Output_ScaleFactor")
-
-    output = output_scale_factor*logits
-
-    output_loss = tf.reduce_mean(tf.nn.l2_loss(output - expected_sparse_output))
-
-    # Inference
-    inference_helper = tf.contrib.seq2seq.ScheduledOutputTrainingHelper(decoder_input, decoder_lengths, sampling_probability=1.0)
-
-    # Inference Decoder
-    inference_decoder = tf.contrib.seq2seq.BasicDecoder(cell, inference_helper, encoder_state)
-    #
-    inference_final_outputs, inference_final_state, inference_final_sequence_lengths = tf.contrib.seq2seq.dynamic_decode(inference_decoder)
-    inference_logits = inference_final_outputs.rnn_output*output_scale_factor
-
-
-    reg_loss = 0
-    for tf_var in tf.trainable_variables():
-        if not ("Bias" in tf_var.name or "Output_" in tf_var.name):
-            reg_loss += tf.reduce_mean(tf.nn.l2_loss(tf_var))
-
-    loss = output_loss + lambda_l2_reg * reg_loss
-
-    with tf.variable_scope('Optimizer'):
-        optimizer = tf.train.AdamOptimizer(learning_rate)
-        train_op = optimizer.minimize(loss)
-
-    sess.run(tf.global_variables_initializer())
-
-    # Training
-    train_losses = []
-    test_losses = []
-    for t in range(nb_iters + 1):
-
-        X, Y = generate_x_y_data(isTrain=True, batch_size=batch_size, l_x=encoder_seq_length, l_y=decoder_seq_length)
-
-        decoder_input_np = np.concatenate((X[:, -2:-1, :], Y[:, 1:, :]), axis=1)
-        feed_dict = {encoder_input: X, expected_sparse_output: Y, decoder_input: decoder_input_np, decoder_lengths: [decoder_seq_length]}
-        _, train_loss, logits_np = sess.run([train_op, loss, encoder_output], feed_dict)
-        train_losses.append(train_loss)
-
-        if t % 10 == 0:
-            # Tester
-            X, Y = generate_x_y_data(isTrain=False, batch_size=batch_size, l_x=encoder_seq_length,
-                                     l_y=decoder_seq_length)
-            feed_dict = {encoder_input: X, expected_sparse_output: Y, decoder_input: decoder_input_np,
-                         decoder_lengths: [decoder_seq_length]}
-            _, test_loss, logits_np_test = sess.run([train_op, loss, encoder_output], feed_dict)
-
-            test_losses.append(test_loss)
-            print("Step {}/{}, train loss: {}, \tTEST loss: {}".format(t, nb_iters, train_loss, test_loss))
-
-
-    # inference
-
-
-    X, Y = generate_x_y_data(isTrain=False, batch_size=batch_size, l_x=encoder_seq_length, l_y=decoder_seq_length)
-
-    inference_decoder_input_np = np.concatenate((X[:, -2:-1, :], Y[:, 1:, :]), axis=1)
-    feed_dict = {encoder_input: X, expected_sparse_output: Y, decoder_input: inference_decoder_input_np, decoder_lengths: [decoder_seq_length]}
-
-
-    _, loss_t, inference_logits_np = sess.run([train_op, loss, inference_logits], feed_dict)
-
-
-    print 'Done'
-
-
-
-
-
-
-
-plt.figure(figsize=(12, 6))
-plt.plot(
-    np.array(range(0, len(test_losses))) / float(len(test_losses) - 1) * (len(train_losses) - 1),
-    np.log(test_losses),
-    label="Test loss"
-)
-plt.plot(
-    np.log(train_losses),
-    label="Train loss"
-)
-plt.title("Training errors over time (on a logarithmic scale)")
-plt.xlabel('Iteration')
-plt.ylabel('log(Loss)')
-plt.legend(loc='best')
-plt.show()
-
-
-# Test
-nb_predictions = 50
-print("Let's visualize {} predictions with our signals:".format(nb_predictions))
-
-X, Y = generate_x_y_data(isTrain=False, batch_size=1, l_x=encoder_seq_length, l_y=decoder_seq_length)
-inference_decoder_input_np = np.concatenate((X[:, -2:-1, :], Y[:, 1:, :]), axis=1)
-feed_dict = {encoder_input: X, decoder_input: inference_decoder_input_np, decoder_lengths: [decoder_seq_length]}
-outputs = sess.run(inference_logits, feed_dict)
-
-for j in range(nb_predictions):
-
+    encoder_seq_length += 50
     X, Y = generate_x_y_data(isTrain=False, batch_size=1, l_x=encoder_seq_length, l_y=decoder_seq_length)
-    inference_decoder_input_np = np.concatenate((X[:, -2:-1, :], Y[:, 1:, :]), axis=1)
+    inference_decoder_input_np = np.concatenate((X[:, -1:, :], Y[:, :-1, :]), axis=1)
     feed_dict = {encoder_input: X, decoder_input: inference_decoder_input_np, decoder_lengths: [decoder_seq_length]}
     outputs = sess.run(inference_logits, feed_dict)
 
+    for j in range(nb_predictions):
+
+        X, Y = generate_x_y_data(isTrain=False, batch_size=1, l_x=encoder_seq_length, l_y=decoder_seq_length)
+        inference_decoder_input_np = np.concatenate((X[:, -1:, :], Y[:, :-1, :]), axis=1)
+        feed_dict = {encoder_input: X, decoder_input: inference_decoder_input_np, decoder_lengths: [decoder_seq_length]}
+        outputs = sess.run(inference_logits, feed_dict)
 
 
-    plt.figure(figsize=(12, 3))
 
-    for k in range(1):
-        past = X[0, :, 0]
-        expected = Y[0, :, 0]
-        pred = outputs[0, :, 0]
+        plt.figure(figsize=(12, 3))
 
-        label1 = "Seen (past) values" if k == 0 else "_nolegend_"
-        label2 = "True future values" if k == 0 else "_nolegend_"
-        label3 = "Predictions" if k == 0 else "_nolegend_"
-        plt.plot(range(len(past)), past, "o--b", label=label1)
-        plt.plot(range(len(past), len(expected) + len(past)), expected, "x--b", label=label2)
-        plt.plot(range(len(past), len(pred) + len(past)), pred, "o--y", label=label3)
-        print(pred)
-    plt.legend(loc='best')
-    plt.title("Predictions v.s. true values")
-    plt.show()
+        for k in range(1):
+            past = X[0, :, 0]
+            expected = Y[0, :, 0]
+            pred = outputs[0, :, 0]
 
-print("Reminder: the signal can contain many dimensions at once.")
-print("In that case, signals have the same color.")
-print("In reality, we could imagine multiple stock market symbols evolving,")
-print("tied in time together and seen at once by the neural network.")
+            label1 = "Seen (past) values" if k == 0 else "_nolegend_"
+            label2 = "True future values" if k == 0 else "_nolegend_"
+            label3 = "Predictions" if k == 0 else "_nolegend_"
+            plt.plot(range(len(past)), past, "o--b", label=label1)
+            plt.plot(range(len(past), len(expected) + len(past)), expected, "x--b", label=label2)
+            plt.plot(range(len(past), len(pred) + len(past)), pred, "o--y", label=label3)
+            print(pred)
+        plt.legend(loc='best')
+        plt.title("Predictions v.s. true values")
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
